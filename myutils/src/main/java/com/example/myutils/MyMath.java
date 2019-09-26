2987c8ceaed500f3f987396bffb1ed41eef3d3aa
Jamal <jamal@nakko.com>
2019-09-26 12:07:34 +0200

    Initial commit

---
+++ myutils/src/main/java/com/example/myutils/MyMath.java
@@ -0,0 +1,12 @@
+package com.example.myutils;
+
+public class MyMath {
+
+    public static int Plus(int a, int b) {
+        return a + b;
+    }
+
+    public static int Minus(int a, int b) {
+        return a - b;
+    }
+}
